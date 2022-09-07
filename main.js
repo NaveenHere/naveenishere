6f9bd458e21e75b1cb256d97c6d2244f9d3e063b
NaveenHere <85273777+NaveenHere@users.noreply.github.com>
2022-09-08 00:38:14 +0530

    New files are added

---
+++ main.js
@@ -0,0 +1,73 @@
+// AOS Animation
+AOS.init();
+
+// Hamburger Section
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+// Scroll To Button
+var mybutton = document.getElementById("ScrollBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+var project = $('.project-carousel');
+project.owlCarousel({
+    items:1,
+    margin: 10,
+    loop:true,
+    dots: false,
+    autoHeight:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1,
+        }
+    }
+})
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:5,
+    loop:true,
+    dots: false,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:3,
+        },
+        1000:{
+            items:5,
+        }
+    }
+})
